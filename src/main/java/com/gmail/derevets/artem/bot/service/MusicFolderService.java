fc90b9fb799d6d3ac04f21d62a9c818ed1a0b4a5
Artem <derevets.artem@gmail.com>
2018-08-13 22:55:37 +0300

    fix

--- src/main/java/com/gmail/derevets/artem/bot/service/MusicFolderService.java
+++ src/main/java/com/gmail/derevets/artem/bot/service/MusicFolderService.java
@@ -3,8 +3,6 @@ package com.gmail.derevets.artem.bot.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 
@@ -16,9 +14,11 @@ import java.util.List;
 public class MusicFolderService {
 
 
-    private final Path myDir = Paths.get("D:\\VK audio");
+    private final Path myDir = Paths.get("C:\\Users\\Artem\\Downloads\\VK audio");
 
     private Logger logger = LoggerFactory.getLogger(MusicFolderService.class);
+
+    private List<File> listAudio;
 
     private String fileName;
 
